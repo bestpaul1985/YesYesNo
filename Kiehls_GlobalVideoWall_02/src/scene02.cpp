8ec9c07ceed837c247c985d2505bfd14626e63e7
Paul Cheng <bestpaul1985@gmail.com>
2014-08-12 15:01:31 -0400

    i

--- Kiehls_GlobalVideoWall_02/src/scene02.cpp
+++ Kiehls_GlobalVideoWall_02/src/scene02.cpp
@@ -9,13 +9,6 @@ void scene02::setup(){
 #ifdef _USE_4k_SCREEN
     camWidth = 960;
     camHeight = 540;
-    
-#else
-    camWidth = 960;
-    camHeight = 540;
-    
-#endif
-    
     //---------------------------LUT
     dir.allowExt("cube");
 	dir.listDir("LUTs/");
@@ -36,7 +29,42 @@ void scene02::setup(){
             cout << " - unavailable " << endl;
         }
 	}
+    //------------------------------video grabber
+	vidGrabber.setDeviceID(0);
+	vidGrabber.setDesiredFrameRate(60);
     
+	vidGrabber.setVerbose(true);
+	vidGrabber.initGrabber(camWidth,camHeight);
+    
+    grabWidth = 520;
+    grabHeight = 520;
+    
+    photoData 	= new unsigned char[grabWidth*grabHeight*3];
+    grabTexture.allocate(grabWidth, grabHeight,GL_RGB);
+    
+#else
+    camWidth = 960;
+    camHeight = 540;
+    //---------------------------LUT
+    dir.allowExt("cube");
+	dir.listDir("LUTs/");
+	dir.sort();
+    dirLoadIndex=0;
+    loadLUT(dir.getPath(dirLoadIndex));
+	
+	lutImg.allocate(camWidth, camHeight, OF_IMAGE_COLOR);
+    lutPos.set(-lutImg.getWidth()*0.5f, lutImg.getHeight()*0.5f, 0);
+    
+    //we can now get back a list of devices.
+	vector<ofVideoDevice> devices = vidGrabber.listDevices();
+    for(int i = 0; i < devices.size(); i++){
+		cout << devices[i].id << ": " << devices[i].deviceName;
+        if( devices[i].bAvailable ){
+            cout << endl;
+        }else{
+            cout << " - unavailable " << endl;
+        }
+	}
     //------------------------------video grabber
 	vidGrabber.setDeviceID(0);
 	vidGrabber.setDesiredFrameRate(60);
@@ -61,6 +89,12 @@ void scene02::setup(){
     shader.load("photobooth_shaders/noise.vert", "photobooth_shaders/noise.frag");
     myFbo.allocate(camWidth, camHeight, GL_RGBA, 4);
     shaderFbo.allocate(camWidth, camHeight, GL_RGBA, 4);
+#endif
+    
+  
+    
+    
+
 }
 
 //--------------------------------------------------------------
@@ -103,7 +137,9 @@ void scene02::update(){
 //--------------------------------------------------------------
 
 void scene02::draw(){
-    myFbo.begin();
+  
+    
+//    myFbo.begin();
     
     ofPushMatrix();
     ofTranslate(ofGetWidth()*0.5f, ofGetHeight()*0.5f, 0);
@@ -127,48 +163,48 @@ void scene02::draw(){
     }
     ofPopMatrix();
     
-    myFbo.end();
+//    myFbo.end();
     
 //    shaderFbo.begin();
-    shader.begin();
-    shader.setUniform2f("mouse", mouseX, mouseY);
-    ofSetColor(255);
-    myFbo.draw(0, 0);
-    shader.end();
+//    shader.begin();
+//    shader.setUniform2f("mouse", mouseX, mouseY);
+//    ofSetColor(255);
+//    myFbo.draw(0, 0);
+//    shader.end();
 //    shaderFbo.end();
 
 //    ofSetColor(255);
 //    shaderFbo.draw(0,0);
     //----------------------------------------- grab cam data
     
-    int k = 0;
-    ofPixels pix;
-    myFbo.readToPixels(pix);
-    
-    for (int i = 0; i < shaderFbo.getWidth(); i++){
-        for (int j=0; j< shaderFbo.getHeight(); j++) {
-            
-            if (i > ((shaderFbo.getWidth() - grabWidth)*0.5f)  && i <= ((shaderFbo.getWidth() - grabWidth)*0.5f + grabWidth) && j > (shaderFbo.getHeight()-grabHeight)*0.5f && j <= ((shaderFbo.getHeight()-grabHeight)*0.5f+grabHeight)) {
-                int ii = j * camWidth + i;
-                ofColor c = pix.getColor(i, j);
-                photoData[k * 3] = c.r;
-                photoData[k * 3 + 1] = c.g;
-                photoData[k * 3 + 2] = c.b;
-                k ++;
-            }
-        }
-    }
-    grabTexture.loadData(photoData, grabWidth,grabHeight, GL_RGB);
-    
-    ofPushMatrix();
-    ofTranslate(ofGetWidth()*0.5f, ofGetHeight()*0.5f);
-    ofRotateZ(90);
-    ofSetColor(255);
-    grabTexture.draw(-grabTexture.getWidth()/2, grabTexture.getHeight()/2, grabTexture.getWidth(), -grabTexture.getHeight());
-    ofPopMatrix();
-    
-    ofSetColor(color);
-    ofRect(0, 0, ofGetWidth(), ofGetHeight());
+//    int k = 0;
+//    ofPixels pix;
+//    myFbo.readToPixels(pix);
+//    
+//    for (int i = 0; i < shaderFbo.getWidth(); i++){
+//        for (int j=0; j< shaderFbo.getHeight(); j++) {
+//            
+//            if (i > ((shaderFbo.getWidth() - grabWidth)*0.5f)  && i <= ((shaderFbo.getWidth() - grabWidth)*0.5f + grabWidth) && j > (shaderFbo.getHeight()-grabHeight)*0.5f && j <= ((shaderFbo.getHeight()-grabHeight)*0.5f+grabHeight)) {
+//                int ii = j * camWidth + i;
+//                ofColor c = pix.getColor(i, j);
+//                photoData[k * 3] = c.r;
+//                photoData[k * 3 + 1] = c.g;
+//                photoData[k * 3 + 2] = c.b;
+//                k ++;
+//            }
+//        }
+//    }
+//    grabTexture.loadData(photoData, grabWidth,grabHeight, GL_RGB);
+//    
+//    ofPushMatrix();
+//    ofTranslate(ofGetWidth()*0.5f, ofGetHeight()*0.5f);
+//    ofRotateZ(90);
+//    ofSetColor(255);
+//    grabTexture.draw(-grabTexture.getWidth()/2, grabTexture.getHeight()/2, grabTexture.getWidth(), -grabTexture.getHeight());
+//    ofPopMatrix();
+//    
+//    ofSetColor(color);
+//    ofRect(0, 0, ofGetWidth(), ofGetHeight());
 
     
 }
