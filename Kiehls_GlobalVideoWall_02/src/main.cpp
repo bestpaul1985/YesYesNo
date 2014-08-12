19999a3742be3033d15316787e71022fa061ac30
Paul Cheng <bestpaul1985@gmail.com>
2014-08-12 12:15:58 -0400

    update

--- Kiehls_GlobalVideoWall_02/src/main.cpp
+++ Kiehls_GlobalVideoWall_02/src/main.cpp
@@ -3,7 +3,7 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(2160/4,3840/4,OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(2160,3840,OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
