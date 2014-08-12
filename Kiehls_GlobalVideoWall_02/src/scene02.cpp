55a1ffbcc65ad5a0964817581444dd85ec3a5951
Paul Cheng <bestpaul1985@gmail.com>
2014-08-12 16:29:41 -0400

    a

--- Kiehls_GlobalVideoWall_02/src/scene02.cpp
+++ Kiehls_GlobalVideoWall_02/src/scene02.cpp
@@ -221,7 +221,7 @@ void scene02::draw(){
     ofTranslate(ofGetWidth()*0.5f, ofGetHeight()*0.5f);
     ofRotateZ(90);
     ofSetColor(255);
-    float scale =  ofGetWidth()/camHeight;
+    float scale =  4;
     grabTexture.draw(-grabTexture.getWidth()/2*scale, grabTexture.getHeight()/2*scale, grabTexture.getWidth()*scale, -grabTexture.getHeight()*scale);
     ofPopMatrix();
     
