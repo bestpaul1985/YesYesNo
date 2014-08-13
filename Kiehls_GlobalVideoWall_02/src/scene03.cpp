f324ab68967006673ead2ea292fab362a08f19de
Paul Cheng <bestpaul1985@gmail.com>
2014-08-13 14:03:01 -0400

    a

--- Kiehls_GlobalVideoWall_02/src/scene03.cpp
+++ Kiehls_GlobalVideoWall_02/src/scene03.cpp
@@ -46,7 +46,7 @@ void scene03::draw(){
 #else
     ofTranslate(ofGetWidth()/2, ofGetHeight()/2-25);
 #endif
-    ofRotateZ(90);
+//    ofRotateZ(90);
     ofSetColor(255);
     photo.draw(-photoWidth*0.5f, -photoHeight*0.5f,photoWidth,photoHeight);
     ofPopMatrix();
