8e0af14924b4f95b1e844742dc89970c28d0d283
Paul Cheng <bestpaul1985@gmail.com>
2014-08-14 14:54:17 -0400

    a

--- Kiehls_GlobalVideoWall_05/src/scene02.cpp
+++ Kiehls_GlobalVideoWall_05/src/scene02.cpp
@@ -192,7 +192,7 @@ void scene02::draw(){
     
     ofRect(0, 0, ofGetWidth(), ofGetHeight());
     ofSetColor(255);
-    float scale = 1.5;
+    float scale = 2.5;
     myFbo.getTextureReference().drawSubsection((ofGetWidth()-grabWidth*scale)*0.5f, 800, grabWidth*scale, grabWidth*scale, 0, 0, grabWidth, grabWidth);
     
     ofSetColor(color);
