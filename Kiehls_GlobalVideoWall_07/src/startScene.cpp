0d30a8072568095cfcc2c910de22bb78bb81ef86
Paul Cheng <bestpaul1985@gmail.com>
2014-08-31 17:12:05 -0400

    a

---
+++ Kiehls_GlobalVideoWall_07/src/startScene.cpp
@@ -0,0 +1,9 @@
+//
+//  startScene.cpp
+//  Kiehls_GlobalVideoWall_07
+//
+//  Created by Peng Cheng on 8/31/14.
+//
+//
+
+#include "startScene.h"
---
+++ Kiehls_GlobalVideoWall_07/src/startScene.h
@@ -0,0 +1,15 @@
+
+#ifndef __Kiehls_GlobalVideoWall_07__startScene__
+#define __Kiehls_GlobalVideoWall_07__startScene__
+
+#include "ofMain.h"
+#include "baseScene.h"
+
+class startScene:public baseScene {
+
+public:
+    
+    void setup(){};
+};
+
+#endif
